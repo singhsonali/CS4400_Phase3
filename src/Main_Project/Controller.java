4c8c500691fbb3267fa055c5d3f791d1b571b913
ashika789 <ashika789@gmail.com>
2016-11-30 10:46:54 -0500

    Linked login to new student registration and main page
    Still need to figure out now to not make a new stage each time and
    reload on same stage.

--- src/Main_Project/Controller.java
+++ src/Main_Project/Controller.java
@@ -7,31 +7,43 @@ import java.io.IOException;
 import java.awt.Button;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
+import java.awt.event.ActionEvent;
 
 public class Controller {
     private Scene root;
     private Stage primaryStage;
 
     @FXML
-    private Button loginBtn;
-    @FXML
-    private Button registerBtn;
-
-    @FXML
-    /**
-     * Sets action for the next button.
-     */
     private void setLoginBtn() {
+
+
         Parent root = null;
         try {
-            root = FXMLLoader.load(getClass().getResource("Login2" +
+            root = FXMLLoader.load(getClass().getResource("MainPage" +
                     ".fxml"));
+            primaryStage = new Stage();
+            primaryStage.setTitle("Group 54 Phase 3");
+            primaryStage.setScene(new Scene(root, 700, 500));
+            primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        primaryStage.setTitle("Group 54 Phase 3");
-        primaryStage.setScene(new Scene(root, 700, 500));
-        primaryStage.show();
+
+
+    }
+
+    @FXML
+    private void setRegistrationBtn() {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("NewStudentRegistration.fxml"));
+            primaryStage = new Stage();
+            primaryStage.setTitle("Group 54 Phase 3");
+            primaryStage.setScene(new Scene(root, 700, 500));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- src/Main_Project/Main.java
+++ src/Main_Project/Main.java
@@ -1,12 +1,15 @@
 package Main_Project;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -15,7 +18,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
