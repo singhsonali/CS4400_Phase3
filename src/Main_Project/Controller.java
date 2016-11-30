ff41d33b9c4ed3c9397a7272601420dcf8bc92ce
ashika789 <ashika789@gmail.com>
2016-11-29 19:53:42 -0500

    Finished setup in IntelliJ with javafx + login screen done

---
+++ src/Main_Project/Controller.java
@@ -0,0 +1,11 @@
+package Main_Project;
+
+import javafx.fxml.FXML;
+import java.awt.Button;
+
+public class Controller {
+    @FXML
+    private Button loginBtn;
+    @FXML
+    private Button registerBtn;
+}
