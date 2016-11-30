12516a724dcb2007baf95aefe33f730ff79ac9fd
ashika789 <ashika789@gmail.com>
2016-11-29 19:21:47 -0500

    Setting up initial folders on eclipse/javafx

---
+++ Phase3/src/LoginController.java
@@ -0,0 +1,15 @@
+
+import java.io.Serializable;
+
+import javafx.scene.control.*;
+
+public class LoginController implements Serializable  {
+	
+//	@FXML
+//	private Button loginBtn;
+//	
+//	@FXML
+//	private Button registerBtn;
+//	
+	
+}
