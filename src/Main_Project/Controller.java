ffbf72944c517132bbf2ddfbaecd20b5875bba5e
ashika789 <ashika789@gmail.com>
2016-11-29 20:26:23 -0500

    Add code for login next button

--- src/Main_Project/Controller.java
+++ src/Main_Project/Controller.java
@@ -1,11 +1,37 @@
 package Main_Project;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import java.io.IOException;
 import java.awt.Button;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 public class Controller {
+    private Scene root;
+    private Stage primaryStage;
     @FXML
     private Button loginBtn;
     @FXML
     private Button registerBtn;
+
+    @FXML
+    /**
+     * Sets action for the next button.
+     */
+    private void setLoginBtn() {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Login2" +
+                    ".fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        primaryStage.setTitle("Group 54 Phase 3");
+        primaryStage.setScene(new Scene(root, 700, 500));
+        primaryStage.show();
+
+    }
+
 }
