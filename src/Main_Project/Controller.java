0ed1ce732d1cb9c1c82847fec256c818e0240f9e
ashika789 <ashika789@gmail.com>
2016-12-03 11:27:21 -0500

    set error messages on log in screen

--- src/Main_Project/Controller.java
+++ src/Main_Project/Controller.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.io.IOException;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import java.awt.event.ActionEvent;
@@ -28,32 +29,55 @@ public class Controller {
     @FXML
     private TextField password;
 
+    @FXML
+    private Text invalidMsg;
+    @FXML
+    private Text invalidUsername;
+    @FXML
+    private Text invalidPassword;
 
 
     @FXML
     private void setLoginBtn() {
-        System.out.println(username.toString());
-        if (username.equals("")) {
-            System.out.println("No username entered.");
-            username.setText("Please fill in a username!");
-         }
-        else if (password.equals("") ) {
-            System.out.println("No password entered.");
-            password.setText("Please fill in a password!");
-
-        } else {
-
-            Parent root = null;
-            try {
-                root = FXMLLoader.load(getClass().getResource("MainPage" +
-                        ".fxml"));
-                primaryStage.setTitle("Group 54 Phase 3");
-                primaryStage.setScene(new Scene(root));
-                primaryStage.show();
-
-            } catch (IOException e) {
-                e.printStackTrace();
+        System.out.println(username);
+        if (username == null || username.getText().trim().isEmpty()) {
+            if (password != null || !password.getText().trim().isEmpty()) {
+                invalidPassword.setVisible(false);
             }
+            System.out.println("No username entered.");
+//            username.setText("Please fill in a username!");
+            invalidUsername.setVisible(true);
+         }
+
+        else if (password == null || password.getText().trim().isEmpty()) {
+            if (username != null || !username.getText().trim().isEmpty()){
+                invalidUsername.setVisible(false);
+            }
+            System.out.println("No password entered.");
+//            password.setText("Please fill in a password!");
+            invalidPassword.setVisible(true);
+        } else {
+            invalidUsername.setVisible(false);
+            invalidPassword.setVisible(false);
+            //CHECK HERE FOR VALID LOG IN CREDENTIALS
+            boolean isValid = true;
+            if (isValid) {
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("MainPage" +
+                            ".fxml"));
+                    primaryStage.setTitle("Group 54 Phase 3");
+                    primaryStage.setScene(new Scene(root));
+                    primaryStage.show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                invalidMsg.setVisible(true);
+            }
+
         }
 
 
