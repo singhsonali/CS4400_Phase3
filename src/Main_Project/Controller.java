f96f57c9ed44b878be26dacadcbce2389bc20287
ashika789 <ashika789@gmail.com>
2016-12-03 12:06:00 -0500

    Added error messages for registration page!

--- src/Main_Project/Controller.java
+++ src/Main_Project/Controller.java
@@ -59,7 +59,7 @@ public class Controller {
         } else {
             invalidUsername.setVisible(false);
             invalidPassword.setVisible(false);
-            //CHECK HERE FOR VALID LOG IN CREDENTIALS
+            //TODO: CHECK HERE FOR VALID LOG IN CREDENTIALS
             boolean isValid = true;
             if (isValid) {
                 Parent root = null;
@@ -94,8 +94,83 @@ public class Controller {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private Text emailExistsMsg;
+
+    @FXML
+    private TextField gtEmailAddr;
+
+    @FXML
+    private TextField usernameNSR;
+    @FXML
+    private TextField password1NSR;
+    @FXML
+    private TextField password2NSR;
+
+    @FXML
+    private Text invalidUsernameNSR;
+
+    @FXML
+    private Text invalidPasswordNSR;
+
+    @FXML
+    private Text wrongPasswordNSR;
+
+    @FXML
+    private Text invalidgtEmailAddrNSR;
+
+    @FXML
+    private void setCreateBtn() {
+        invalidUsernameNSR.setVisible(false);
+        invalidPasswordNSR.setVisible(false);
+        wrongPasswordNSR.setVisible(false);
+        invalidgtEmailAddrNSR.setVisible(false);
+        Parent root = null;
+        Boolean gtEmailExists = false;
+        if (gtEmailExists) {
+            //TODO: CHECK TO SEE IF DATABASE HAS GT EMAIL ALREADY OR NOT
+            emailExistsMsg.setVisible(true);
+        } else if (usernameNSR == null || usernameNSR.getText().trim()
+                .isEmpty()){
+            //CHECK TO SEE IF WE HAVE AN INPUTTED USERNAME
+            invalidUsernameNSR.setVisible(true);
+            System.out.println("This is the usernameNSR: " + usernameNSR.toString());
+        } else if(gtEmailAddr == null || gtEmailAddr.getText().trim()
+                .isEmpty()){
+            //CHECK TO SEE IF WE HAVE AN INPUTTED PASSWORD
+            invalidUsernameNSR.setVisible(false);
+            invalidgtEmailAddrNSR.setVisible(true);
+
+        } else if(password1NSR == null || password1NSR.getText().trim()
+                .isEmpty()){
+            //CHECK TO SEE IF WE HAVE AN INPUTTED PASSWORD
+            invalidUsernameNSR.setVisible(false);
+            invalidPasswordNSR.setVisible(true);
+
+        } else if (!password1NSR.getText().equals(password2NSR.getText())){
+            //CHECK TO SEE IF WE HAVE MATCHING PASSWORDS
+            invalidgtEmailAddrNSR.setVisible(false);
+            wrongPasswordNSR.setVisible(true);
+        } else {
+            wrongPasswordNSR.setVisible(false);
+//            CHECK TO SEE IF WE CREATED AN ACCOUNT SUCCESSFULLY
+            Boolean success = true;
+            if (success) {
+                System.out.println("Made an account succesfully.");
+            }
+            else {
+                System.out.println("Could not create account successfully...");
+            }
+
+        }
+
 
     }
+
+
+
 
 //    @FXML
 //    private void setAddCategory() {
